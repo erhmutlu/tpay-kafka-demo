2493897b263c917c6b14f2cef66979db5033888b
erhan.mutlu <erhan.mutlu@trendyol.com>
2020-06-01 22:31:34 +0300

    spring-kafka version is set to 2.5.0.RELEASE

--- async-payment-gateway/src/main/java/org/erhanmutlu/asyncpaymentgateway/infrastructure/kafka/interceptor/MessageProducerListener.java
+++ async-payment-gateway/src/main/java/org/erhanmutlu/asyncpaymentgateway/infrastructure/kafka/interceptor/MessageProducerListener.java
@@ -1,6 +1,7 @@
 package org.erhanmutlu.asyncpaymentgateway.infrastructure.kafka.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.erhanmutlu.asyncpaymentgateway.infrastructure.kafka.contract.IdempotentMessage;
 import org.springframework.kafka.support.ProducerListener;
@@ -9,12 +10,12 @@ import org.springframework.kafka.support.ProducerListener;
 public class MessageProducerListener implements ProducerListener<String, IdempotentMessage> {
 
     @Override
-    public void onSuccess(String topic, Integer partition, String key, IdempotentMessage message, RecordMetadata recordMetadata) {
-        log.info("success for writing topic: {} for uniqueId: {}", topic, message.getTrackingNumber());
+    public void onSuccess(ProducerRecord<String, IdempotentMessage> producerRecord, RecordMetadata recordMetadata) {
+        log.info("success for writing topic: {} for uniqueId: {}", producerRecord.topic(), producerRecord.value().getTrackingNumber());
     }
 
     @Override
-    public void onError(String topic, Integer partition, String key, IdempotentMessage message, Exception exception) {
-        log.info("failure for writing topic: {} for uniqueId: {}", topic, message.getTrackingNumber());
+    public void onError(ProducerRecord<String, IdempotentMessage> producerRecord, Exception exception) {
+        log.info("failure for writing topic: {} for uniqueId: {}", producerRecord.topic(), producerRecord.value().getTrackingNumber());
     }
 }
